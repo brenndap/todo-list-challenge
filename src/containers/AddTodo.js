e94ccfb8d00abea904df56b4c88e64a8dc9be208
brenndafarinha <brennda.farinha@elloxdigital.com>
2020-03-06 16:27:16 -0300

    add project

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from '../actions'
+import { Form, Button } from 'antd'
+
+function AddTodo() {
+
+    return (
+        //esse é o componente que deve lidar com a adição de todos
+    )
+}
+
+export default AddTodo
---
+++ src/containers/VisibleTodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleTodo } from '../actions'
+import TodoList from '../components/TodoList'
+
+
+function VisibleTodoList() {
+
+  //componente que mostra as todos adicionadas e cuida da lógica de completar ou não a task
+  
+}
+
+export default VisibleTodoList
